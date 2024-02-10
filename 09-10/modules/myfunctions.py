2466501441f3bc8a84d71fa033f72cb031f2d191
TARIQ JAMIL <72103319+tariqjamil-bwp@users.noreply.github.com>
2024-02-10 17:13:58 +0500

    11

---
+++ 09-10/modules/myfunctions.py
@@ -0,0 +1,16 @@
+def square(number):
+    '''
+    It calculates square of a number.
+    
+    Parameter:
+    Number: A numeric value
+    
+    Return:
+    It returns an numeric value equal to square of the number
+    '''
+    sq = number**2
+    return sq
+
+if __name__ == "__main__":
+    print(square(12))
+
